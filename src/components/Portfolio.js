3b13acf5ae0f8832331a50c2e4561a034953805b
rutvik <rutvikpatel91037@gmail.com>
2025-04-21 16:01:10 +0530

    Image Changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -36,7 +36,7 @@ const Portfolio = () => {
                                 <a type="link" key={p._id} href={`/product/${p._id}`}>
                                     <div className="group rounded-2xl shadow-xl w-auto m-3 transform transition duration-500 hover:scale-110 portofolio-card">
                                         <div className="relative">
-                                            <img src={'https://i.ibb.co/Xhn9yG1/vanvet.jpg'} alt="Portfolio" className="rounded-t-2xl z-0" />
+                                            <img src={p.image} alt={p.title} className="rounded-t-2xl z-0" />
                                             <div className="absolute flex w-full h-full top-0 opacity-0 bg-black justify-center rounded-t-2xl rounded-b img-hover">
                                                 <button className="focus:outline-none">
                                                     <svg className="w-20 h-20 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
